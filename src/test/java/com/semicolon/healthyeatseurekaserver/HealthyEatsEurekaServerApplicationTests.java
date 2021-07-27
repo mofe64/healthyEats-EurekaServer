934cdc95c1bb6abc62476b397f554fb079ff483d
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2021-07-27 11:45:41 +0100

    eureka server setup

---
+++ src/test/java/com/semicolon/healthyeatseurekaserver/HealthyEatsEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semicolon.healthyeatseurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthyEatsEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
